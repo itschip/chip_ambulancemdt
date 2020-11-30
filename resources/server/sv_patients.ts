47440cae3b925db97a32929daec9754d9365969f
Christopher <onlygamernerd@gmail.com>
2020-11-30 21:53:44 +0100

    tweak(patinets): limit on query for fetching filterd patinents

--- resources/server/sv_patients.ts
+++ resources/server/sv_patients.ts
@@ -6,7 +6,7 @@ import { IPatient } from "../../web/src/interfaces/patients";
 
 async function getFilteredPatients(search: string): Promise<any> {
   const query =
-    "SELECT firstname, lastname, dateofbirth, bloodtype, avatar, phone_number, doctor FROM users WHERE lower(firstname) LIKE lower(?) OR lower(lastname) LIKE lower(?)";
+    "SELECT firstname, lastname, dateofbirth, bloodtype, avatar, phone_number, doctor FROM users WHERE lower(firstname) LIKE lower(?) OR lower(lastname) LIKE lower(?) LIMIT 50";
   const [results] = await pool.query(query, [search, search]);
   const patients = results;
   return patients;
